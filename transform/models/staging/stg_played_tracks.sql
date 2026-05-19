ad27f82dd166ef40aff0814ff32afc4e6b7e1d01
Viktor <viktorbjorklind2000@gmail.com>
2026-05-19 18:50:12 +0200

    add dbt staging model for played_tracks

---
+++ transform/models/staging/stg_played_tracks.sql
@@ -0,0 +1,20 @@
+with source as (
+    select * from {{ source('spotify', 'played_tracks') }}
+),
+
+renamed as (
+    select
+        played_at,
+        track_id,
+        track_name,
+        artist_name,
+        album_name,
+        duration_ms,
+        round(duration_ms / 60000.0, 2) as duration_minutes,
+        date(played_at)                  as played_date,
+        extract(hour from played_at)     as played_hour,
+        extract(dow from played_at)      as day_of_week
+    from source
+)
+
+select * from renamed
